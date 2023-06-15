3bd971cf9b6a322d7a2dfe498f456fce6d820056
ytroyr <77324175+ytroyr@users.noreply.github.com>
2023-06-15 10:00:34 +1000

    Sum_subsequence.py looped

--- Sum_subsequence.py
+++ Sum_subsequence.py
@@ -1,6 +1,7 @@
-start = int(input("Введите начальное число: "))
-stop = int(input("Введите конечное число: "))
-number_sum = 0
-for number in range(start, stop + 1):
-  number_sum += number
-print("Сумма чисел от", start, "до", stop, "равна", number_sum)
+While True:
+  start = int(input("Введите начальное число: "))
+  stop = int(input("Введите конечное число: "))
+  number_sum = 0
+  for number in range(start, stop + 1):
+    number_sum += number
+  print("Сумма чисел от", start, "до", stop, "равна", number_sum)
