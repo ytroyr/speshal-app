0f687d06063816fd6709ce02de423be86ad8265a
Anatoly Borzdov <toliksaturn3@gmail.com>
2023-06-17 11:31:44 +1000

    renamed variables

--- Sum_subsequence.py
+++ Sum_subsequence.py
@@ -1,7 +1,8 @@
 while True:
-  start = int(input("Введите начальное число: "))
-  stop = int(input("Введите конечное число: "))
+  start_n = int(input("Введите начальное число: "))
+  stop_n = int(input("Введите конечное число: "))
   number_sum = 0
-  for number in range(start, stop + 1):
+  for number in range(start_n, stop_n + 1):
     number_sum += number
-  print("Сумма чисел от", start, "до", stop, "равна", number_sum)
+  print("Сумма чисел от", start_n, "до", stop_n
+        , "равна", number_sum)
